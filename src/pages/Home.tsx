67bd202fcf82977435f1f87f7a24ce3582643a90
oumansourabdallah202-ctrl <oumansourabdallah202@gmail.com>
2026-02-27 18:23:38 +0100

    Pass data to FeaturedPhones and ArticleContent to avoid runtime crashes

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -4,13 +4,29 @@ import FeaturedPhones from '../components/FeaturedPhones';
 import ArticleContent from '../components/ArticleContent';
 import CategoryList from '../components/CategoryList';
 import NewsletterForm from '../components/NewsletterForm';
+import { phones } from '../data/phones';
+
+// sample article data for the homepage
+const sampleArticle = {
+    title: 'Welcome to AbduTech!',
+    author: 'AbduTech Team',
+    date: 'February 27, 2026',
+    featuredImage: '/images/phone-a.jpg',
+    content: '<p>Stay up to date with the latest phone technology news and reviews.</p>',
+};
 
 const Home: React.FC = () => {
     return (
         <div className="home">
             <Hero />
-            <FeaturedPhones />
-            <ArticleContent />
+            <FeaturedPhones phones={phones} />
+            <ArticleContent
+                title={sampleArticle.title}
+                author={sampleArticle.author}
+                date={sampleArticle.date}
+                featuredImage={sampleArticle.featuredImage}
+                content={sampleArticle.content}
+            />
             <CategoryList />
             <NewsletterForm />
         </div>
